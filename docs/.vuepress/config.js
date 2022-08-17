07a7aa3fca71c00204b0ccf86531d416381d50ad
Cyberaurora <22611640+cyberaurora@users.noreply.github.com>
2022-08-17 18:33:46 +0800

    update galois

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -18,7 +18,7 @@ module.exports = {
       { text: "Fusotao", link: "https://www.fusotao.org" },
       { text: "Github", link: "https://github.com/uinb" },
     ],
-    sidebar: ["/", "substrate-network", "core-features","rest-api"],
+    sidebar: ["/", "substrate-network", "core-features","how-to-run-dex-upon-fusotao"],
     sidebarDepth: 3,
     smoothScroll: true,
     displayAllHeaders:true
